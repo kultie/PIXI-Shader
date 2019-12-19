de45b46f94e7019a767ca56e5f75fd4818a40806
longnt <longnt1@kaopiz.com>
2019-12-19 15:57:21 +0700

    Remove redundant files

--- shiny.glsl
+++
@@ -1,9 +0,0 @@
-  precision mediump float;
-  
-  varying vec2 vTextureCoord;//The coordinates of the current pixel
-  uniform sampler2D uSampler;//The image data
-
-  void main(void) {
-    gl_FragColor = texture2D(uSampler, vTextureCoord);
-    gl_FragColor.r = 0.0;
-  }
